660ba0e8ddce40ff277defe65570348f57cf1d55
Cédric Ackermann <ackermanncedi@gmail.com>
2024-06-14 08:24:44 +0200

    Push for battino

--- src/db.rs
+++ src/db.rs
@@ -1,12 +1,35 @@
+use diesel::pg::PgConnection;
 use diesel::prelude::*;
-use diesel::sqlite::SqliteConnection;
 use dotenvy::dotenv;
 use std::env;
 
-pub fn establish_connection() -> SqliteConnection {
+use crate::schema::setting;
+use crate::Setting;
+
+pub fn establish_connection() -> PgConnection {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    SqliteConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+
+    let connection = PgConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
+
+    println!("Connection to the database established!");
+
+    return connection;
+}
+
+pub fn get_settings()  {
+   let connection = &mut establish_connection();
+    let results = setting::dsl::setting
+        .select(Setting::as_select())
+        .limit(5)
+        .load(connection)
+        .expect("Error loading menucards");
+    for result in &results {
+        println!("{}", result.setting_id);
+        println!("{}", result.id_menucard_active);
+        println!("{}", result.restaurant_width);
+        println!("{}", result.restaurant_height);
+    }
 }
