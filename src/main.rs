2daceaced54b0aee83e13e690b4599e26b66d137
Battino <battino46@gmail.com>
2024-06-20 13:35:35 +0200

    working demo on menu card

--- src/main.rs
+++ src/main.rs
@@ -13,37 +13,6 @@ mod rocket;
 mod schema;
 
 fn main() {
-    /* let mut db_connection = establish_connection();
-    let settings = get_db_settings(&mut db_connection);
-    entities::menucard::get_menucard(&mut db_connection, 1, vec!["dishes"]) */
-    expansions_to_layered(vec![
-        "dish.hello.yes",
-        "dish.hello.no",
-        "epic.hello.bro",
-        "dish",
-        "dish.yes",
-    ]);
-}
-
-fn expansions_to_layered(expansions: Vec<&str>) -> Vec<Vec<&str>> {
-    // Split expansions by dots into a vector of vectors
-    let expasions_split_by_dot: Vec<_> = expansions
-        .iter()
-        .map(|expansion| expansion.split(".").collect::<Vec<&str>>())
-        .collect();
-    let num_rows = expasions_split_by_dot.len();
-    let num_cols = expasions_split_by_dot.get(0).map_or(0, |row| row.len());
-
-    let mut layered_vector: Vec<Vec<&str>> = vec![Vec::with_capacity(num_rows); num_cols];
-
-    for (i, row) in expasions_split_by_dot.iter().enumerate() {
-        for (j, &col) in row.iter().enumerate() {
-            layered_vector[j].push(col);
-        }
-    }
-    for vec in &mut layered_vector {
-        let mut seen = HashSet::new();
-        vec.retain(|&x| seen.insert(x) && !x.is_empty());
-    }
-    return layered_vector;
+    let mut db_connection = establish_connection();
+    rocket::main()
 }
--- src/rocket.rs
+++ src/rocket.rs
@@ -1,12 +1,85 @@
-/* #![feature(proc_macro_hygiene, decl_macro)]
+use std::borrow::Borrow;
 
-#[macro_use] extern crate rocket;
+use rocket::serde::json::Json;
+use rocket::*;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+use crate::{
+    db::establish_connection, endpoint_models,
+    entities::menucard::get_menucard as get_menucard_by_id,
+};
+
+#[get("/setting?<name>")]
+fn get_setting(name: Option<Vec<String>>) -> String {
+    let name = name.unwrap_or_default();
+    let name = name.join(", ");
+    format!("Names: {}", name)
+}
+/* #[get("/menucard/<id>?<expands>")]
+fn get_menucard(id: i32, expands: Option<Vec<&str>>) -> Json<endpoint_models::Menucard> {
+    println!(
+        "Expansions: {}",
+        expands.clone().unwrap_or_default().join(", ")
+    );
+    let menucard =
+        get_menucard_by_id(&mut establish_connection(), id, &expands.unwrap_or(vec![])).unwrap();
+    Json::from(menucard)
+} */
+
+#[derive(FromForm)]
+struct QueryParams {
+    expands: Option<Vec<String>>,
 }
 
-fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
-} */
+#[get("/menucard/<id>?<query..>")]
+fn get_menucard(id: i32, query: QueryParams) -> Json<endpoint_models::Menucard> {
+    let expands: Vec<&str> = query
+        .expands
+        .as_ref()
+        .map(|s| s.iter().flat_map(|v| v.split(',')).collect())
+        .unwrap_or_default();
+
+    println!("Expansions: {}", expands.join(", "));
+
+    let menucard = get_menucard_by_id(&mut establish_connection(), id, &expands).unwrap();
+    Json::from(menucard)
+}
+
+#[get("/dish")]
+fn get_dish() -> &'static str {
+    "Dish"
+}
+#[get("/tag")]
+fn get_tag() -> &'static str {
+    "Tag"
+}
+
+#[get("/table")]
+fn get_table() -> &'static str {
+    "Table"
+}
+
+#[get("/reservation")]
+fn get_reservation() -> &'static str {
+    "Reservation"
+}
+
+#[get("/person")]
+fn get_person() -> &'static str {
+    "Person"
+}
+
+#[launch]
+pub fn rocket() -> _ {
+    rocket::build().mount(
+        "/",
+        routes![
+            get_setting,
+            get_menucard,
+            get_dish,
+            get_tag,
+            get_table,
+            get_reservation,
+            get_person,
+        ],
+    )
+}
