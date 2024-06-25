226ab035b0045f4de0d94989fcbd086440f001ab
Battino <battino46@gmail.com>
2024-06-25 15:33:17 +0200

    add all remaining GET endpoints

--- src/entities/person.rs
+++ src/entities/person.rs
@@ -1,17 +1,139 @@
+//! This module provides functionality for retrieving persons and their related data from the database.
+//!
+//! # Functions
+//! - `get_person`: Retrieves a single person by its ID, including optional expansions for reservations.
+//! - `get_all_persons`: Retrieves all persons, including optional expansions for reservations.
+//! - `expand_reservations`: Helper function to expand reservations for a person.
+
 use diesel::{dsl::insert_into, ExpressionMethods, PgConnection, RunQueryDsl};
 use rocket::serde::json::Json;
 
-use crate::{
-    db::establish_connection,
-    inserteable_models, models,
-    schema::{self, person},
-};
+use crate::{inserteable_models, models};
+
+use crate::endpoint_models;
+use crate::schema::person;
+use crate::schema::reservation::{self};
+use diesel::prelude::*;
+use diesel::result::Error;
+
+use super::reservation::get_reservation;
+
+/// Retrieves a single person by its ID, including optional expansions for reservations.
+///
+/// # Arguments
+///
+/// * `conn` - A mutable reference to a PostgreSQL connection.
+/// * `id` - The ID of the person to retrieve.
+/// * `expansions` - A vector of strings specifying which related data to expand.
+///
+/// # Returns
+///
+/// A result containing the person endpoint model or a Diesel error.
+pub fn get_person(
+    conn: &mut PgConnection,
+    id: i32,
+    expansions: &Vec<&str>,
+) -> Result<endpoint_models::Person, Error> {
+    let models_person: models::Person = person::dsl::person
+        .find(id)
+        .select(models::Person::as_select())
+        .first::<models::Person>(conn)?;
+
+    // expand reservations
+    let reservations = expand_reservations(conn, id, expansions);
+
+    let endpoints_person: endpoint_models::Person = endpoint_models::Person {
+        person_id: models_person.person_id,
+        name: models_person.name,
+        phone: models_person.phone,
+        reservations: reservations,
+    };
+    Ok(endpoints_person)
+}
+
+/// Retrieves all persons, including optional expansions for reservations.
+///
+/// # Arguments
+///
+/// * `conn` - A mutable reference to a PostgreSQL connection.
+/// * `expansions` - A vector of strings specifying which related data to expand.
+///
+/// # Returns
+///
+/// A result containing a vector of person endpoint models or a Diesel error.
+pub fn get_all_persons(
+    conn: &mut PgConnection,
+    expansions: &Vec<&str>,
+) -> Result<Vec<endpoint_models::Person>, Error> {
+    let models_persons = person::dsl::person
+        .select(models::Person::as_select())
+        .load::<models::Person>(conn)?;
+    let entrypoints_persons = models_persons
+        .iter()
+        .map(|p| {
+            // expand reservations
+            let reservations = expand_reservations(conn, p.person_id, expansions);
+
+            let endpoints_person: endpoint_models::Person = endpoint_models::Person {
+                person_id: p.person_id,
+                name: p.name.clone(),
+                phone: p.phone.clone(),
+                reservations: reservations,
+            };
+            endpoints_person
+        })
+        .collect();
+    Ok(entrypoints_persons)
+}
+
+/*   Expansions   */
+
+/// Helper function to expand reservations for a person.
+///
+/// # Arguments
+///
+/// * `conn` - A mutable reference to a PostgreSQL connection.
+/// * `person_id` - The ID of the person for which to expand reservations.
+/// * `expansions` - A vector of strings specifying which related data to expand.
+///
+/// # Returns
+///
+/// An option containing a vector of reservation endpoint models.
+fn expand_reservations(
+    conn: &mut PgConnection,
+    person_id: i32,
+    expansions: &Vec<&str>,
+) -> Option<Vec<endpoint_models::Reservation>> {
+    if !expansions.contains(&"reservations") {
+        return None;
+    }
+
+    let reservation_expansions: Vec<&str> = expansions
+        .iter()
+        .filter(|e| e.starts_with("reservations."))
+        .map(|e| &e[13..])
+        .collect();
+
+    // Get reservations
+    let relations: Vec<models::Reservation> = reservation::dsl::reservation
+        .filter(reservation::dsl::id_person.eq(person_id))
+        .select(models::Reservation::as_select())
+        .load::<models::Reservation>(conn)
+        .unwrap();
+
+    Some(
+        relations
+            .iter()
+            .map(|rel| get_reservation(conn, rel.reservation_id, &reservation_expansions).unwrap())
+            .collect(),
+    )
+}
 
 pub fn insert_person(
     conn: &mut PgConnection,
     person: Json<inserteable_models::Person>,
 ) -> models::Person {
-    insert_into(schema::person::dsl::person)
+    insert_into(person::dsl::person)
         .values((
             person::name.eq(person.name.clone()),
             person::phone.eq(person.phone.clone()),
--- src/entities/reservation.rs
+++ src/entities/reservation.rs
@@ -1,17 +1,173 @@
+//! This module provides functionality for retrieving reservations and their related data from the database.
+//!
+//! # Functions
+//! - `get_reservation`: Retrieves a single reservation by its ID, including optional expansions for tables and person.
+//! - `get_all_reservations`: Retrieves all reservations, including optional expansions for tables and person.
+//! - `expand_tables`: Helper function to expand tables for a reservation.
+//! - `expand_person`: Helper function to expand person for a reservation.
+
 use diesel::{dsl::insert_into, ExpressionMethods, PgConnection, RunQueryDsl};
 use rocket::serde::json::Json;
 
-use crate::{
-    db::establish_connection,
-    inserteable_models, models,
-    schema::{self, reservation},
-};
+use crate::inserteable_models;
+use crate::schema::table::{self};
+use crate::schema::{reservation, table_reservation};
+use crate::{endpoint_models, models};
+
+use diesel::prelude::*;
+use diesel::result::Error;
+
+use super::person::get_person;
+use super::table::get_table;
+
+/// Retrieves a single reservation by its ID, including optional expansions for tables and person.
+///
+/// # Arguments
+///
+/// * `conn` - A mutable reference to a PostgreSQL connection.
+/// * `id` - The ID of the reservation to retrieve.
+/// * `expansions` - A vector of strings specifying which related data to expand.
+///
+/// # Returns
+///
+/// A result containing the reservation endpoint model or a Diesel error.
+pub fn get_reservation(
+    conn: &mut PgConnection,
+    id: i32,
+    expansions: &Vec<&str>,
+) -> Result<endpoint_models::Reservation, Error> {
+    let models_reservation: models::Reservation = reservation::dsl::reservation
+        .find(id)
+        .select(models::Reservation::as_select())
+        .first::<models::Reservation>(conn)?;
+
+    // expand tables and person
+    let tables = expand_tables(conn, id, expansions);
+    let person = expand_person(conn, models_reservation.id_person, expansions);
+
+    let endpoints_reservation: endpoint_models::Reservation = endpoint_models::Reservation {
+        reservation_id: models_reservation.reservation_id,
+        id_person: models_reservation.id_person,
+        start_timestamp: models_reservation.start_timestamp,
+        end_timestamp: models_reservation.end_timestamp,
+        person_count: models_reservation.person_count,
+        tables: tables,
+        person: person,
+    };
+    Ok(endpoints_reservation)
+}
+
+/// Retrieves all reservations, including optional expansions for tables and person.
+///
+/// # Arguments
+///
+/// * `conn` - A mutable reference to a PostgreSQL connection.
+/// * `expansions` - A vector of strings specifying which related data to expand.
+///
+/// # Returns
+///
+/// A result containing a vector of reservation endpoint models or a Diesel error.
+pub fn get_all_reservations(
+    conn: &mut PgConnection,
+    expansions: &Vec<&str>,
+) -> Result<Vec<endpoint_models::Reservation>, Error> {
+    let models_reservations = reservation::dsl::reservation
+        .select(models::Reservation::as_select())
+        .load::<models::Reservation>(conn)?;
+    let entrypoints_reservations = models_reservations
+        .iter()
+        .map(|r| {
+            // expand tables and person
+            let tables = expand_tables(conn, r.reservation_id, expansions);
+            let person = expand_person(conn, r.id_person, expansions);
+
+            let endpoints_reservation: endpoint_models::Reservation =
+                endpoint_models::Reservation {
+                    reservation_id: r.reservation_id,
+                    id_person: r.id_person,
+                    start_timestamp: r.start_timestamp,
+                    end_timestamp: r.end_timestamp,
+                    person_count: r.person_count,
+                    tables: tables,
+                    person: person,
+                };
+            endpoints_reservation
+        })
+        .collect();
+    Ok(entrypoints_reservations)
+}
+
+/*   Expansions   */
+
+/// Helper function to expand tables for a reservation.
+///
+/// # Arguments
+///
+/// * `conn` - A mutable reference to a PostgreSQL connection.
+/// * `reservation_id` - The ID of the reservation for which to expand tables.
+/// * `expansions` - A vector of strings specifying which related data to expand.
+///
+/// # Returns
+///
+/// An option containing a vector of table endpoint models.
+fn expand_tables(
+    conn: &mut PgConnection,
+    reservation_id: i32,
+    expansions: &Vec<&str>,
+) -> Option<Vec<endpoint_models::Table>> {
+    if !expansions.contains(&"tables") {
+        return None;
+    }
+
+    let table_expansions: Vec<&str> = expansions
+        .iter()
+        .filter(|e| e.starts_with("tables."))
+        .map(|e| &e[7..])
+        .collect();
+
+    // Get tables
+    let relations: Vec<models::TableReservation> = table_reservation::dsl::table_reservation
+        .filter(table_reservation::dsl::id_reservation.eq(reservation_id))
+        .select(models::TableReservation::as_select())
+        .load::<models::TableReservation>(conn)
+        .unwrap();
+
+    Some(
+        relations
+            .iter()
+            .map(|rel| get_table(conn, rel.id_table, &table_expansions).unwrap())
+            .collect(),
+    )
+}
+
+/// Helper function to expand person for a reservation.
+///
+/// # Arguments
+///
+/// * `conn` - A mutable reference to a PostgreSQL connection.
+/// * `person_id` - The ID of the person for which to expand.
+/// * `expansions` - A vector of strings specifying which related data to expand.
+///
+/// # Returns
+///
+/// An option containing the person endpoint model.
+fn expand_person(
+    conn: &mut PgConnection,
+    person_id: i32,
+    expansions: &Vec<&str>,
+) -> Option<endpoint_models::Person> {
+    if !expansions.contains(&"person") {
+        return None;
+    }
+
+    get_person(conn, person_id, expansions).ok()
+}
 
 pub fn insert_reservation(
     conn: &mut PgConnection,
     reservation: Json<inserteable_models::Reservation>,
 ) -> models::Reservation {
-    insert_into(schema::reservation::dsl::reservation)
+    insert_into(reservation::dsl::reservation)
         .values((
             reservation::id_person.eq(reservation.id_person.clone()),
             reservation::start_timestamp.eq(reservation.start_timestamp.clone()),
