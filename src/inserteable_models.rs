f7bf2a902f8191018ac0c5434206d262a2831a10
Battino <battino46@gmail.com>
2024-06-26 08:06:02 +0200

    add additional insertable_models

--- src/inserteable_models.rs
+++ src/inserteable_models.rs
@@ -10,8 +10,6 @@ pub struct Person {
     pub phone: String,
 }
 
-
-
 #[derive(Deserialize, JsonSchema)]
 pub struct Reservation {
     pub id_person: i32,
@@ -20,4 +18,44 @@ pub struct Reservation {
     #[serde(deserialize_with = "json_date::deserialize")]
     pub end_timestamp: NaiveDateTime,
     pub person_count: i32,
+}
+
+#[derive(Deserialize, JsonSchema)]
+pub struct Dish {
+    pub name: String,
+    pub description: String,
+    pub dish_type: String,
+}
+
+#[derive(Deserialize, JsonSchema)]
+pub struct Tag {
+    pub name: String,
+}
+
+#[derive(Deserialize, JsonSchema)]
+pub struct DishTag {
+    pub id_dish: i32,
+    pub id_tag: i32,
+}
+
+#[derive(Deserialize, JsonSchema)]
+pub struct MenucardDish {
+    pub id_menucard: i32,
+    pub id_dish: i32,
+    pub chefs_choice: bool,
+}
+
+#[derive(Deserialize, JsonSchema)]
+pub struct Table {
+    pub seat_count: i32,
+    pub coord_x: i32,
+    pub coord_y: i32,
+    pub width: i32,
+    pub height: i32,
+}
+
+#[derive(Deserialize, JsonSchema)]
+pub struct TableReservation {
+    pub id_table: i32,
+    pub id_reservation: i32,
 }
